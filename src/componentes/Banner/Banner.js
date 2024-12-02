4b3242de6f679a2c1d0ea11d8c31e45cf933177a
Kaio <kaiohtf38@gmail.com>
2024-12-02 10:50:54 -0300

    criando o repositório

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+function Banner(){
+    return (
+        <header className="banner">
+            <img src="..\imagens\imagens\banner.png" alt="O banner principal da página"></img>
+        </header>
+    )
+}
+
+export default Banner
